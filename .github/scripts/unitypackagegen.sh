083df981df048ca357427bf1d3e5a9f62a33bbdd
cnlohr <lohr85@gmail.com>
2025-11-21 00:06:17 -0800

    Add jigglephysics to the unitypackage

--- .github/scripts/unitypackagegen.sh
+++ .github/scripts/unitypackagegen.sh
@@ -48,6 +48,7 @@ if [[ "$1" == "full" ]]; then
               Packages/com.basis.profilerintergration:
               Packages/com.gator-dragon-games.jigglephysics:
               Packages/com.avionblock.opussharp:
+              Packages/UnityJigglePhysics-upm:
               Assets/Resource:
               Assets/StreamingAssets:
               Assets/Plugins:
